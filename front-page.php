caa81f3fae647efec93377a2fc894bbd8f160b7a
Kyle Beard <kylebeard@nuancedmedia.com>
2016-02-18 21:52:48 -0700

    removing some boilerplate and reorganizing styles

--- front-page.php
+++ front-page.php
@@ -5,6 +5,7 @@
 
 
 add_action('genesis_after_header', 'an_do_hero');
+add_action('genesis_after_header', 'an_do_portfolio');
 function an_do_hero() {
 	?>
 	<section class="hero" style="background-image:url(/wp-content/themes/genesis-allnighter/images/hero.jpg);">
@@ -12,6 +13,11 @@ function an_do_hero() {
 			<h1>ux / ui / front-end development</h1>
 		</div>
 	</section>
+	<?php
+}
+
+function an_do_portfolio () {
+	?>
 	<section class="section-portfolio">
 		<h2 class="section-headline">Portfolio</h2>
         <div class="portfolio-items">
@@ -20,7 +26,7 @@ function an_do_hero() {
 					<h5>Portfolio Item #1</h5>
 					<div class="portfolio-links">
 						<a class="link-www" href="#">
-							<i class="fa fa-globe"></i>
+							<i class="fa fa-globe"></i> View Website
 						</a>
 					</div>
 				</div>
@@ -29,7 +35,6 @@ function an_do_hero() {
 	</section>
 	<?php
 }
-
 
 
 
