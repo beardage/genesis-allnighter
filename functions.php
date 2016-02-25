b0ea24f961f4652d10f180797ca41c8efcf23e1e
Kyle Beard <kylebeard@nuancedmedia.com>
2016-02-24 22:31:51 -0700

    adding html 5 support and removing title from homepage

--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@ require_once( get_template_directory() . '/lib/init.php' );
 define( 'CHILD_THEME_NAME', 'Allnighter' );
 define( 'CHILD_THEME_URL', 'http://www.kylebeard.com/' );
 
+// html5
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
 // Require the Lib stuff
 require_once( get_stylesheet_directory() . '/lib/cpts/portfolio-cpt.php' );
 
