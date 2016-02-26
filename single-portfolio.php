a4a8a9a24e8e90a2241749757d6bd113201e5486
Kyle Beard <kylebeard@nuancedmedia.com>
2016-02-26 02:25:17 -0700

    established markup format for blog pages, added body font, styling blog single

--- single-portfolio.php
+++ single-portfolio.php
@@ -7,4 +7,7 @@ remove_action('genesis_entry_footer', 'genesis_entry_footer_markup_close', 15);
 remove_action('genesis_entry_footer', 'genesis_post_meta');
 remove_action( 'genesis_before_post_content', 'genesis_post_info' );
 
+//* Remove the entry title in the entry header
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
 genesis();
