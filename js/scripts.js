7cf991613ca906d4d612fd09618353b3c3cad374
Kyle Beard <kylebeard@nuancedmedia.com>
2016-03-16 00:11:14 -0700

    adding randomized glitch effect to navigation

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,14 @@ jQuery(function() {
 	activateNavigationEffect();
 });
 
+
+// add data attribute for navigation glitch effect
 function activateNavigationEffect () {
 	jQuery('#menu-primary-navigation .menu-item a').each(function(e) {
-		jQuery(this).attr('text', jQuery(this).text());
+		jQuery(this).attr('data-text', jQuery(this).text());
+	});
+	jQuery('#menu-primary-navigation .menu-item a').on('hover', function() {
+		jQuery(this).toggleClass('glitch');
+
 	});
 }
