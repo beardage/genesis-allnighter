1cfcda2eb49a9b84b8afe1191972bfa1296b7d25
Kyle Beard <kylebeard@nuancedmedia.com>
2016-05-20 21:48:27 -0700

    adding template for blog-single

--- archive.php
+++ archive.php
@@ -1,4 +1,3 @@
-
 <?php
 
 //* Remove the post meta function
@@ -9,9 +8,12 @@
 // remove_action( 'genesis_before_post_content', 'genesis_post_info' );
 
 //* Remove the entry title in the entry header
-//remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
-
+add_action('genesis_after_header', 'an_blog_header');
+function an_blog_header() {
+	echo "<h1>Blog</h1>";
+}
 
 
 genesis();
